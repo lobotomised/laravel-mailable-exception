7b45b2597c5d3f21ca6ccd691bb85d025725da1d
lobotomised <lobotomised@lobotomised.com>
2025-01-20 13:51:31 +0100

    Not call Illuminate\Foundation\Application->environment()

--- src/LaravelMailableException.php
+++ src/LaravelMailableException.php
@@ -13,7 +13,7 @@ class LaravelMailableException
         $allowedEnvironments = config('mailable-exception.allowed_environments');
         $to = config('mailable-exception.to.address');
 
-        if (! app()->environment($allowedEnvironments)) {
+        if(! in_array(config('app.env'), $allowedEnvironments)) {
             return;
         }
 
